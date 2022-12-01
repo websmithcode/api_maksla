e9da74c32260613c0db7ea5ac103970e6c1cca15
alxnsmith <alx.n.smith@ya.ru>
2022-12-01 20:03:14 +0300

    Prepare for uploading data via keybased access api

--- tests/Feature/UploadViaAPITest.php
+++ tests/Feature/UploadViaAPITest.php
@@ -2,15 +2,40 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
 use Tests\TestCase;
 
 class UploadViaAPITest extends TestCase
 {
-				public function testEndpointIsReacheble()
-				{
-								$response = $this->get('/api/upload');
-								$response->assertStatus(200);
-				}
+    public function setup(): void
+    {
+        parent::setup();
+        $this->fixture_path = storage_path('docs/works.json');
+    }
+
+    public function testEndpointAccess()
+    {
+        $get_response = $this->get('/api/upload');
+        $get_response->assertStatus(405);
+
+        $post_response = $this->post('/api/upload');
+        $post_response->assertStatus(403);
+
+        // Send file
+        $file = $this->getUploadableFile($this->fixture_path);
+        $upload_post_response = $this->call('POST', '/api/upload', [], [], ['file' => $file]);
+        $upload_post_response->assertStatus(403);
+    }
+
+   protected function getUploadableFile($file, $mime_type = 'text/json')
+   {
+       $path = $file;
+       $original_name = basename($path);
+       $error = null;
+       $test = true;
+
+       $file = new UploadedFile($path, $original_name, $mime_type, $error, $test);
+
+       return $file;
+   }
 }
